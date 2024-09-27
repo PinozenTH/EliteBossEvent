69dda581894b7d22b5c7e837502728abb83736a3
pinont <pinozen.thailand@gmail.com>
2024-09-27 23:23:24 +0700

    - fixed maximum player not being check during early connection to server

--- src/main/java/com/pinont/elitebossevent/Listeners/PlayerListener.java
+++ src/main/java/com/pinont/elitebossevent/Listeners/PlayerListener.java
@@ -4,10 +4,13 @@ import com.pinont.elitebossevent.EliteBossEvent;
 import com.pinont.elitebossevent.Tasks.SummonMobTask;
 import com.pinont.elitebossevent.Utils.Message.Reply;
 import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 
 public class PlayerListener implements Listener {
 
@@ -16,21 +19,24 @@ public class PlayerListener implements Listener {
 
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event) {
-        if (Bukkit.getOnlinePlayers().size() == 1) {
-            new Reply(Reply.SenderType.CONSOLE, "Minimum players reached, Elite event is resuming!");
-            main.executeTask();
+        if (Bukkit.getOnlinePlayers().isEmpty() || main.getConfig().getInt("elite-boss-event.min-players") > Bukkit.getOnlinePlayers().size()) {
+            if (Bukkit.getOnlinePlayers().size() == 1) {
+                new Reply(Reply.SenderType.CONSOLE, "Minimum players reached, Elite event is resuming!");
+                main.executeTask();
+            }
+            summonMobTask.updateMaxTickedPlayers();
         }
-        summonMobTask.updateMaxTickedPlayers();
     }
 
     @EventHandler
     public void onPlayerQuit(PlayerQuitEvent event) {
-        if (Bukkit.getOnlinePlayers().isEmpty() || main.getConfig().getInt("elite-boss-event.min-players") > Bukkit.getOnlinePlayers().size()) {
-            new Reply(Reply.SenderType.CONSOLE, "Online players are less than minimum players required, Elite event is pausing!");
-            summonMobTask.stop();
-            return;
-        }
-        summonMobTask.updateMaxTickedPlayers();
+        Bukkit.getScheduler().runTask(main, () -> {
+            if (Bukkit.getOnlinePlayers().isEmpty() || main.getConfig().getInt("elite-boss-event.min-players") > Bukkit.getOnlinePlayers().size()) {
+                new Reply(Reply.SenderType.CONSOLE, "Online players are less than minimum players required, Elite event is pausing!");
+                summonMobTask.stop();
+            }
+            summonMobTask.updateMaxTickedPlayers();
+        });
     }
 
 }
